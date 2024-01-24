e2663b5026c2f2f901b7b929e4f43a2c7edddfd0
Fabien B <fabien.benaitier@gmail.com>
2024-01-24 23:57:34 +0100

    FInished clean transition of sections

--- controller.js
+++ controller.js
@@ -133,9 +133,41 @@ const content = new Swiper(".swiper.content", {
 	mousewheel: {
 		forceToAxis: true,
 		sensitivity: 5
+	},
+	effect: "creative",
+  	creativeEffect: {
+        prev: {
+          translate: ["-100%", 0, 0]
+        },
+        next: {
+          translate: ["100%", 0, 0]
+        },
+    }
+});
+
+
+
+
+
+const avoid_flicker_effect = function(swiper_obj, prev_slide_index, current_slide_index, next_slide_index){
+
+	// Setting all slides to a 0 opacity except the 2 transitioning
+	swiper_obj.slides.forEach((slide) => {
+
+		slide.style.visibility = "hidden";
+
+	});
+
+	swiper_obj.slides[prev_slide_index].style.visibility = "visible";
+	swiper_obj.slides[current_slide_index].style.visibility = "visible";
+
+	if( next_slide_index ){
+		swiper_obj.slides[next_slide_index].style.visibility = "visible";
 	}
 
-});
+}
+
+
 
 
 content.on("activeIndexChange", (event) => {
@@ -144,7 +176,31 @@ content.on("activeIndexChange", (event) => {
 
 	set_active_menus(menu_level_1, menu_level_2, menu_submenu_index);
 
+
+	// Avoid having slides that move in the background
+	const current_slide_index = content.activeIndex ? content.activeIndex : 0;
+
+	const prev_slide_index = content.previousIndex ? content.previousIndex : 0;
+
+	avoid_flicker_effect(content, prev_slide_index, current_slide_index);
+
 });
+
+
+content.on("setTransition", (event) => {
+
+	const current_slide_index = content.activeIndex;
+
+	const prev_slide_index = (current_slide_index - 1) >= 0 ? current_slide_index - 1 : 0;
+
+	const next_slide_index = (current_slide_index + 1) <= (content.slides.length - 1) ? current_slide_index + 1 : current_slide_index;
+
+	avoid_flicker_effect(content, prev_slide_index, current_slide_index, next_slide_index);
+
+});
+
+
+
 
 
 // Showing the right content section when clicking on a menu item
