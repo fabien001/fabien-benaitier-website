787d5c5709451548b79ecae05e6fc4847bf6ca0f
Fabien B <fabien.benaitier@gmail.com>
2024-01-26 20:10:13 +0100

    Added mechanism to fetch content from html files

--- content-controller.js
+++ content-controller.js
@@ -2,24 +2,115 @@
 
 
 
+const content_files_paths = [
+
+	"1.1@what-i-do.html",
+	"1.2@my-experience.html",
+	"1.3@apart-from-that.html",
+	"2.1@this-website.html",
+	"2.2@discover.html",
+	"2.3@conceptualize.html",
+	"2.4@implement.html",
+	"3.1@personas.html",
+	"3.2@automatic-alerts.html",
+	"3.3@digital-workstation.html",
+	"3.4@desyre.html",
+	"3.5@phev-coach.html",
+	"3.6@ds-smart-touch.html",
+	"4.1@we.html",
+	"4.2@ship.html",
+	"4.3@code.html",
+	"4.4@to-users.html",
+	"4.5@for-value.html",
+	"5.1@links.html",
+	"5.2@contact.html",
+	"5.3@paper-resume.html",
+	"5.4@paper-portfolio.html"
+
+];
+
+
+// --------------- ↓ Creating content slides ↓ ---------------
+const swiper_content = document.querySelector(".swiper.content .swiper-wrapper");
+
+const content_slide_template = document.getElementById("content-slide-template");
+
+
+const append_content_slide = function(swiper_wrapper, template, raw_html_content, menu_index){
+
+	const swiper_slide = template.content.cloneNode(true);
+
+	swiper_slide.querySelector(".section").setAttribute("menu-index", menu_index);
+
+	var parser = new DOMParser();
+	var html_content = parser.parseFromString(raw_html_content, 'text/html').body.firstChild;
+
+	swiper_slide.querySelector(".content-container").appendChild(html_content);
+
+	swiper_wrapper.appendChild(swiper_slide);
+
+};
+
+
+
+
+const all_raw_html_content = [];
+
+content_files_paths.forEach(async function(content_file_path) {
+
+
+	all_raw_html_content.push(fetch(`./content-pages/${content_file_path}`).then(function (response) {
+
+		const raw_html_content = response.text();
+
+		return raw_html_content;
+
+	}).catch((error) => {
+
+		console.log(error);
+
+	}));
+
+});
+
+
+Promise.all(all_raw_html_content).then((all_raw_html_content) => {
+
+	all_raw_html_content.forEach((raw_html_content, index) => {
+
+		const menu_index = content_files_paths[index].split("@")[0];
+
+		append_content_slide(swiper_content, content_slide_template, raw_html_content, menu_index);
+
+	});
+
+
+	// Initiating the swiper when all slides created
+	const content_slides_created = new Event("content-slides-created");
+
+	document.dispatchEvent(content_slides_created);
+
+
+});
+// --------------- ↑ Creating content slides ↑ ---------------
+
+
+
+
+
+
+
 
 
 // --------------- ↓ Content scrolling using locomotive scroll ↓ ---------------
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    tablet: {
-	    breakpoint: 0
-  	},
-  	mobile: {
-	    breakpoint: 0
-  	}
-});
-
-
-scroll.on("scroll", (event) => {
-
-	// console.log(event);
-
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('[data-scroll-container]'),
+//     smooth: true,
+//     tablet: {
+// 	    breakpoint: 0
+//   	},
+//   	mobile: {
+// 	    breakpoint: 0
+//   	}
+// });
 // --------------- ↑ Content scrolling using locomotive scroll ↑ ---------------
--- nav-controller.js
+++ nav-controller.js
@@ -126,114 +126,120 @@ menus.forEach( (menu) => {
 
 
 // ---------------- ↓ Content swiper controller ↓ ----------------
-const content = new Swiper(".swiper.content", {
+// Activate the content swiper only when all content is fetch and slides created
+document.addEventListener("content-slides-created", () => {
 
-	direction: "horizontal",
-	slidesPerView: 1,
-	mousewheel: {
-		forceToAxis: true,
-		sensitivity: 5
-	},
-	effect: "creative",
-  	creativeEffect: {
-        prev: {
-          translate: ["-100%", 0, 0]
-        },
-        next: {
-          translate: ["100%", 0, 0]
-        },
-    },
-    nested: true
-});
+	const content = new Swiper(".swiper.content", {
 
-
-
-
-
-const avoid_flicker_effect = function(swiper_obj, prev_slide_index, current_slide_index, next_slide_index){
-
-	// Setting all slides to a 0 opacity except the 2 transitioning
-	swiper_obj.slides.forEach((slide) => {
-
-		slide.style.visibility = "hidden";
-
+		direction: "horizontal",
+		slidesPerView: 1,
+		mousewheel: {
+			forceToAxis: true,
+			sensitivity: 5
+		},
+		effect: "creative",
+	  	creativeEffect: {
+	        prev: {
+	          translate: ["-100%", 0, 0]
+	        },
+	        next: {
+	          translate: ["100%", 0, 0]
+	        },
+	    },
+	    nested: true
 	});
 
-	swiper_obj.slides[prev_slide_index].style.visibility = "visible";
-	swiper_obj.slides[current_slide_index].style.visibility = "visible";
 
-	if( next_slide_index ){
-		swiper_obj.slides[next_slide_index].style.visibility = "visible";
+
+
+
+	const avoid_flicker_effect = function(swiper_obj, prev_slide_index, current_slide_index, next_slide_index){
+
+		// Setting all slides to a 0 opacity except the 2 transitioning
+		swiper_obj.slides.forEach((slide) => {
+
+			slide.style.visibility = "hidden";
+
+		});
+
+		swiper_obj.slides[prev_slide_index].style.visibility = "visible";
+		swiper_obj.slides[current_slide_index].style.visibility = "visible";
+
+		if( next_slide_index ){
+			swiper_obj.slides[next_slide_index].style.visibility = "visible";
+		}
+
 	}
 
-}
 
 
 
+	content.on("activeIndexChange", (event) => {
 
-content.on("activeIndexChange", (event) => {
+		const menu_submenu_index = content.slides[content.activeIndex].getAttribute("menu-index");
 
-	const menu_submenu_index = content.slides[content.activeIndex].getAttribute("menu-index");
-
-	set_active_menus(menu_level_1, menu_level_2, menu_submenu_index);
-
-
-	// Avoid having slides that move in the background
-	const current_slide_index = content.activeIndex ? content.activeIndex : 0;
-
-	const prev_slide_index = content.previousIndex ? content.previousIndex : 0;
-
-	avoid_flicker_effect(content, prev_slide_index, current_slide_index);
-
-});
+		set_active_menus(menu_level_1, menu_level_2, menu_submenu_index);
 
 
-content.on("setTransition", (event) => {
+		// Avoid having slides that move in the background
+		const current_slide_index = content.activeIndex ? content.activeIndex : 0;
 
-	const current_slide_index = content.activeIndex;
+		const prev_slide_index = content.previousIndex ? content.previousIndex : 0;
 
-	const prev_slide_index = (current_slide_index - 1) >= 0 ? current_slide_index - 1 : 0;
-
-	const next_slide_index = (current_slide_index + 1) <= (content.slides.length - 1) ? current_slide_index + 1 : current_slide_index;
-
-	avoid_flicker_effect(content, prev_slide_index, current_slide_index, next_slide_index);
-
-});
-
-
-
-
-
-// Showing the right content section when clicking on a menu item
-// And set active the right menu/submenu
-
-Array.from(menu_level_1.querySelectorAll("li")).reduce((accumulator, menu_level_1_item, index) => {
-
-	const fixed_acc = accumulator;
-
-	menu_level_1_item.addEventListener("click", () => {
-
-		content.slideTo(fixed_acc, slide_to_timing);
+		avoid_flicker_effect(content, prev_slide_index, current_slide_index);
 
 	});
 
-	Array.from(menu_level_2.slides[index].querySelectorAll("li")).forEach((menu_level_2_item, index_2) => {
+
+	content.on("setTransition", (event) => {
+
+		const current_slide_index = content.activeIndex;
+
+		const prev_slide_index = (current_slide_index - 1) >= 0 ? current_slide_index - 1 : 0;
+
+		const next_slide_index = (current_slide_index + 1) <= (content.slides.length - 1) ? current_slide_index + 1 : current_slide_index;
+
+		avoid_flicker_effect(content, prev_slide_index, current_slide_index, next_slide_index);
+
+	});
+
+
+
+
+
+	// Showing the right content section when clicking on a menu item
+	// And set active the right menu/submenu
+
+	Array.from(menu_level_1.querySelectorAll("li")).reduce((accumulator, menu_level_1_item, index) => {
 
 		const fixed_acc = accumulator;
 
-		menu_level_2_item.addEventListener("click", () => {
+		menu_level_1_item.addEventListener("click", () => {
 
 			content.slideTo(fixed_acc, slide_to_timing);
 
 		});
 
-		accumulator += 1;
+		Array.from(menu_level_2.slides[index].querySelectorAll("li")).forEach((menu_level_2_item, index_2) => {
 
-	});
+			const fixed_acc = accumulator;
 
-	return accumulator;
+			menu_level_2_item.addEventListener("click", () => {
 
-}, 0);
+				content.slideTo(fixed_acc, slide_to_timing);
+
+			});
+
+			accumulator += 1;
+
+		});
+
+		return accumulator;
+
+	}, 0);
+
+});
+// End of content slides created eventListener
 
 
 // ---------------- ↑ Content swiper controller ↑ ----------------
